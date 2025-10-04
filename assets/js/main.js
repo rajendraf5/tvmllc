4b4e72148c12e90a7f3e8760d7926ec9990d3a39
rajendraf5 <princesvishwakarmaf5@gmail.com>
2025-10-04 18:58:17 +0530

    home page about section
    home page about section design

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,7 +124,18 @@
   }
 }
 
-
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
 
 // DOMContentLoaded  end
 
